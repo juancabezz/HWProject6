638f346e5f934088a036ae99acf858d122dbf59c
juancabeza <juancabeza@localhost>
2023-07-28 23:17:47 -0400

    Initial Commit

---
+++ src/birthday/BdayDate.java
@@ -0,0 +1,11 @@
+package birthday;
+
+public class BdayDate {
+	
+	public static void main(String[] args) {
+		
+	System.out.println("My birthday is on September 27th...");
+	System.out.println("I'm 22 years old");
+	}
+
+}
